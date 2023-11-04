8bd8ebf2379425a9fce9c8cd6ecbb4948fb0668b
khs <hskang200@gmail.com>
2023-11-04 12:53:23 +0900

    change timeFunction

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let startTime = new Date().getTime(); //(자바스크립트 실행 시작시간)
+let timeTaken; // 페이지 로딩에 걸린 시간을 저장할 변수
 
 window.onload = function () {
   let endTime = new Date().getTime(); // 페이지가 완전히 로드된 시간
-  let timeTaken = endTime - startTime; // 페이지 로딩에 걸린 시간
+  timeTaken = endTime - startTime; // 페이지 로딩에 걸린 시간
 
   // 시간을 초로 변환하여 표시
   let timeInSeconds = (timeTaken / 1000).toFixed(2);
@@ -15,15 +16,17 @@ window.onload = function () {
 
 function updateTime() {
   const currentTime = new Date();
+  const adjustedTime = new Date(currentTime.getTime() + timeTaken); // 현재 시간에 페이지 로딩 시간을 더함
+
   const userLang = navigator.language || navigator.userLanguage;
   const timeLabels = userLang.startsWith("ko")
     ? { hour: "시", minute: "분", second: "초", millisecond: "ms" }
     : { hour: "h", minute: "m", second: "s", millisecond: "ms" };
 
-  const hours = String(currentTime.getHours()).padStart(2, "0");
-  const minutes = String(currentTime.getMinutes()).padStart(2, "0");
-  const seconds = String(currentTime.getSeconds()).padStart(2, "0");
-  const milliseconds = String(currentTime.getMilliseconds()).padStart(3, "0");
+  const hours = String(adjustedTime.getHours()).padStart(2, "0");
+  const minutes = String(adjustedTime.getMinutes()).padStart(2, "0");
+  const seconds = String(adjustedTime.getSeconds()).padStart(2, "0");
+  const milliseconds = String(adjustedTime.getMilliseconds()).padStart(3, "0");
 
   document.getElementById("hours").textContent = hours;
   document.getElementById("hour-label").textContent = timeLabels.hour;
